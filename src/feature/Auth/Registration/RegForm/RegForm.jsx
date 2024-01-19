066fbf64d1801178ae0416600f8d324bf3af8e98
don336 <smgmartinez92@gmail.com>
2024-01-19 16:52:27 +0300

    initial code for the registered code

--- src/feature/Auth/Registration/RegForm/RegForm.jsx
+++ src/feature/Auth/Registration/RegForm/RegForm.jsx
@@ -1,13 +1,21 @@
-import { Container, Grid } from "@mui/material";
+import { Container, Grid, Stack } from "@mui/material";
 import { TextField } from "../../../../elements/TextField/index";
-
+import { StyledButton } from "../../../../elements/Button/Button.styles";
 import { Form } from "formik";
 
 const RegForm = () => {
   return (
     <Container component="main" maxWidth="xs">
       <Form sx={{ mt: 3 }} component="form">
-        <h1>Fill In Your Profile</h1>
+        <h1
+          style={{
+            margin: "auto",
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          Fill In Your Profile
+        </h1>
         <Grid container spacing={2}>
           <Grid item xs={12} sm={12}>
             <TextField
@@ -36,6 +44,18 @@ const RegForm = () => {
               placeholder="Enter Password"
               autoComplete="new-password"
             />
+          </Grid>
+          <Grid item xs={12}>
+            <Stack
+              direction={"row"}
+              gap={5}
+              justifyContent={"center"}
+              alignContent={"center"}
+              m={10}
+            >
+              <StyledButton variant="contained">Back</StyledButton>
+              <StyledButton variant="contained">Next</StyledButton>
+            </Stack>
           </Grid>
         </Grid>
       </Form>
